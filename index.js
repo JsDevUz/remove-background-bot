e62abc08dcf97a4a0b540dde227fe32f5b7bb4f8
modos <modos.m98@gmail.com>
2021-08-24 21:34:57 +0430

    add files

---
+++ index.js
@@ -0,0 +1,40 @@
+const FormData = require('form-data');
+const axios = require('axios');
+const { Telegraf } = require('telegraf')
+const config = require('./config.json')
+
+const bot = new Telegraf(config.BOT_TOKEN)
+
+const removeBg = async function (url) {
+
+  const formData = new FormData();
+  formData.append('image_url', url)
+
+  const res = await axios.post('https://api.remove.bg/v1.0/removebg', formData, {
+
+    headers: {...formData.getHeaders(), 'X-Api-Key': 'MzBBpHTJYuzsMhu1eRuZ1hpf'},
+    responseType: 'arraybuffer'
+  })
+
+  return res.data
+
+}
+
+
+bot.command('start', (ctx) =>  {
+  ctx.reply('hello to modos coding channel')
+})
+
+bot.on('photo', async (ctx) => {
+
+  const file_id = ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id
+  const file_path = (await ctx.telegram.getFile(file_id)).file_path
+  const url = `https://api.telegram.org/file/bot${config.BOT_TOKEN}/${file_path}`
+
+
+  const photo = await removeBg(url)
+  ctx.replyWithDocument({ source: photo, filename: 'test.png' })
+  
+})
+
+bot.launch()
