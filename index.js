1e0788440155e2324de9cd66550724f9103f3bad
proger <shakhislom.org@gmail.com>
2023-02-13 10:11:03 +0500

    last

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const removeBg = async function (url) {
 
   const res = await axios.post('https://api.remove.bg/v1.0/removebg', formData, {
 
-    headers: {...formData.getHeaders(), 'X-Api-Key': config.REMOVE_BG_TOKEN},
+    headers: { ...formData.getHeaders(), 'X-Api-Key': config.REMOVE_BG_TOKEN },
     responseType: 'arraybuffer'
   })
 
@@ -21,20 +21,20 @@ const removeBg = async function (url) {
 }
 
 
-bot.command('start', (ctx) =>  {
-  ctx.reply('hello to modos coding channel')
+bot.command('start', (ctx) => {
+  ctx.reply('Hi send me photo!')
 })
 
 bot.on('photo', async (ctx) => {
-
+  bot.telegram.sendMessage(ctx.from.id, 'Working...', { reply_to_message_id: ctx.message.message_id })
   const file_id = ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id
   const file_path = (await ctx.telegram.getFile(file_id)).file_path
   const url = `https://api.telegram.org/file/bot${config.BOT_TOKEN}/${file_path}`
 
 
   const photo = await removeBg(url)
-  ctx.replyWithDocument({ source: photo, filename: 'test.png' })
-  
+  ctx.replyWithDocument({ source: photo, filename: '@rmbackbot.png' })
+
 })
 
 bot.launch()
